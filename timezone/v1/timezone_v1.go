d04ab80e4c0726aeeba6c72ec9d14a8f64bb4a4c
parinyapt <parinyapt99@gmail.com>
2023-04-18 22:59:45 +0700

    edit func global timezone config

--- timezone/v1/timezone_v1.go
+++ timezone/v1/timezone_v1.go
@@ -1,15 +1,22 @@
 package PTGUtimezone
 
 import (
-	"log"
+	"fmt"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
-func GlobalTimezoneSetup(tz string) {
+func GlobalTimezoneSetup(tz string) error {
+	if tz == "" {
+		return errors.New("[Info][PTGUtimezone][TimezoneSetup()]->Timezone Config is empty, using default timezone")
+	}
+
 	loc, err := time.LoadLocation(tz) 
 	if err != nil {
-		log.Fatalf("[Error][PTGUtimezone][TimezoneSetup()]->Failed to LoadLocation Timezone Config : %s", err)
+		return errors.New(fmt.Sprintf("[Error][PTGUtimezone][TimezoneSetup()]->Failed to LoadLocation Timezone Config : %s", err))
 	}
 	time.Local = loc // -> this is setting the global timezone
+	return nil
 	// https://stackoverflow.com/questions/54363451/setting-timezone-globally-in-golang
 }
