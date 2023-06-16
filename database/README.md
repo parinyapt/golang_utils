4657e33ab57bce8f9dec5986eb6d6fdf390cfcbe
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-06-16 16:09:20 +0700

    add Utils for enum in PostgreSQL

---
+++ database/README.md
@@ -0,0 +1,25 @@
+# PTGU Database
+
+## Import
+```go
+import (
+	PTGUjwt "github.com/parinyapt/golang_utils/database/v1"
+)
+```
+
+## Example
+### Generate SQL Command to Create, Drop, Auto Migrate for PostgreSQL v1
+```go
+type AdditionalClaim struct {
+	AccountID string
+}
+
+func main() {
+  type EnumList struct {
+    TestEnum1 any `enumName:"testenum1" enumValue:"status1,status2,status3"`
+    TestEnum2 any `enumName:"testenum2" enumValue:"status1,status2,status3"`
+    TestEnum3 any `enumValue:"status1,status2,status3"` // If not set enumName, it will use field name
+    TestEnum4 any `enumName:"testenum4"` // Required enumValue and it must be more than 1 value and separated by comma (,)
+  }
+}
+```
---
+++ database/v1/database_postgresql_v1.go
@@ -0,0 +1,39 @@
+package PTGUdatabase
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+)
+
+func PostgresqlGenerateCreateTypeEnum(enumName string, enumValues []string) string {
+	var enumValuesString string
+	for _, enumValue := range enumValues {
+		enumValuesString += "'" + enumValue + "',"
+	}
+	return "CREATE TYPE " + enumName + " AS ENUM (" + enumValuesString[:len(enumValuesString)-1] + ");"
+}
+
+func PostgresqlGenerateDropTypeEnum(enumName string) string {
+	return "DROP TYPE " + enumName + ";"
+}
+
+func PostgresqlGenerateAutoMigrateEnum(selectStruct interface{}) (string, error) {
+	var sqlcmd string = ""
+	values := reflect.ValueOf(selectStruct)
+	types := values.Type()
+	for i := 0; i < values.NumField(); i++ {
+		enumNameTemp := types.Field(i).Tag.Get("enumName")
+		if enumNameTemp == "" {
+			enumNameTemp = types.Field(i).Name
+		}
+		enumValueTemp := strings.Split(types.Field(i).Tag.Get("enumValue"), ",")
+		if len(enumValueTemp) == 1 || enumValueTemp[0] == "" {
+			return "", errors.New("[Error][PTGUdatabase][PostgresqlGenerateAutoMigrateEnum()]->Some Enum Value is Empty")
+		}
+		sqlcmd += PostgresqlGenerateDropTypeEnum(enumNameTemp)
+		sqlcmd += PostgresqlGenerateCreateTypeEnum(enumNameTemp, enumValueTemp)
+	}
+
+	return sqlcmd, nil
+}
