e315e8d61115f18a9f7fdd8cf9cd6721496ce85d
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-06-19 11:50:02 +0700

    add new enum auto migrate function and edit create and drop default enum function

--- database/README.md
+++ database/README.md
@@ -16,8 +16,8 @@ type AdditionalClaim struct {
 
 func main() {
   type EnumList struct {
-    TestEnum1 any `enumName:"testenum1" enumValue:"status1,status2,status3"`
-    TestEnum2 any `enumName:"testenum2" enumValue:"status1,status2,status3"`
+    TestEnum1 any `enumName:"testenum1" enumValue:"status1,status2,status3" enumTable:"table1" enumColumn:"type1"`
+    TestEnum2 any `enumName:"testenum2" enumValue:"status1,status2,status3" enumTable:"table2" enumColumn:"type2"`
     TestEnum3 any `enumValue:"status1,status2,status3"` // If not set enumName, it will use field name
     TestEnum4 any `enumName:"testenum4"` // Required enumValue and it must be more than 1 value and separated by comma (,)
   }
--- database/v1/database_postgresql_v1.go
+++ database/v1/database_postgresql_v1.go
@@ -1,15 +1,19 @@
 package PTGUdatabase
 
 import (
-	"errors"
+	"fmt"
 	"reflect"
 	"strings"
 )
 
-func PostgresqlGenerateCreateTypeEnum(enumName string, enumValues []string) string {
+func PostgresqlGenerateCreateTypeEnum(enumName string, enumValues []string, addSingleQoute bool) string {
 	var enumValuesString string
 	for _, enumValue := range enumValues {
-		enumValuesString += "'" + enumValue + "',"
+		if addSingleQoute {
+			enumValuesString += "''" + enumValue + "'',"
+		}else{
+			enumValuesString += "'" + enumValue + "',"
+		}
 	}
 	return "CREATE TYPE " + enumName + " AS ENUM (" + enumValuesString[:len(enumValuesString)-1] + ");"
 }
@@ -18,7 +22,27 @@ func PostgresqlGenerateDropTypeEnum(enumName string) string {
 	return "DROP TYPE IF EXISTS " + enumName + ";"
 }
 
-func PostgresqlGenerateAutoMigrateEnum(selectStruct interface{}) (string, error) {
+func PostgresqlGenerateChangeTypeEnum(enumName string, enumTable string, enumColumn string, enumDefaultValue string) string {
+	return "ALTER TABLE IF EXISTS " + enumTable + " ALTER COLUMN " + enumColumn + " TYPE " + enumName + " USING " + enumColumn + "::text::" + enumName + ";"
+}
+
+func PostgresqlGenerateRenameTypeEnum(enumNameOld string, enumNameNew string) string {
+	return "ALTER TYPE " + enumNameOld + " RENAME TO " + enumNameNew + ";"
+}
+
+func PostgresqlGenerateDropColumnDefaultValue(enumTable string, enumColumn string) string {
+	return "ALTER TABLE IF EXISTS " + enumTable + " ALTER COLUMN " + enumColumn + " DROP DEFAULT;"
+}
+
+func PostgresqlGenerateAddColumnDefaultValue(enumTable string, enumColumn string, enumDefaultValue string, addSingleQoute bool) string {
+	if addSingleQoute {
+		return "ALTER TABLE IF EXISTS " + enumTable + " ALTER COLUMN " + enumColumn + " SET DEFAULT ''" + enumDefaultValue + "'';"
+	}else{
+		return "ALTER TABLE IF EXISTS " + enumTable + " ALTER COLUMN " + enumColumn + " SET DEFAULT '" + enumDefaultValue + "';"
+	}
+}
+
+func PostgresqlGenerateAutoMigrateEnum(tablePrefix string, selectStruct interface{}) (string, error) {
 	var sqlcmd string = ""
 	values := reflect.ValueOf(selectStruct)
 	types := values.Type()
@@ -29,10 +53,38 @@ func PostgresqlGenerateAutoMigrateEnum(selectStruct interface{}) (string, error)
 		}
 		enumValueTemp := strings.Split(types.Field(i).Tag.Get("enumValue"), ",")
 		if len(enumValueTemp) == 1 || enumValueTemp[0] == "" {
-			return "", errors.New("[Error][PTGUdatabase][PostgresqlGenerateAutoMigrateEnum()]->Some Enum Value is Empty")
+			return "", fmt.Errorf("[Error][PTGUdatabase][PostgresqlGenerateAutoMigrateEnum()]-> Enum Value of field %s is Empty", enumNameTemp)
 		}
-		sqlcmd += PostgresqlGenerateDropTypeEnum(enumNameTemp)
-		sqlcmd += PostgresqlGenerateCreateTypeEnum(enumNameTemp, enumValueTemp)
+		enumTableTemp := types.Field(i).Tag.Get("enumTable")
+		if enumTableTemp == "" {
+			return "", fmt.Errorf("[Error][PTGUdatabase][PostgresqlGenerateAutoMigrateEnum()]-> Enum Table of field %s is Empty", enumNameTemp)
+		}
+		enumColumnTemp := types.Field(i).Tag.Get("enumColumn")
+		if enumColumnTemp == "" {
+			return "", fmt.Errorf("[Error][PTGUdatabase][PostgresqlGenerateAutoMigrateEnum()]-> Enum Column of field %s is Empty", enumNameTemp)
+		}
+
+		sqlcmd += `
+		DO $$
+		BEGIN
+				IF EXISTS (SELECT 1 FROM pg_tables WHERE tablename = '` + tablePrefix + enumTableTemp + `') THEN
+		`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateDropColumnDefaultValue(tablePrefix + enumTableTemp, enumColumnTemp) + `';`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateCreateTypeEnum(enumNameTemp + "_new", enumValueTemp, true) + `';`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateChangeTypeEnum(enumNameTemp + "_new", tablePrefix + enumTableTemp, enumColumnTemp, enumValueTemp[0]) + `';`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateDropTypeEnum(enumNameTemp) + `';`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateRenameTypeEnum(enumNameTemp + "_new", enumNameTemp) + `';`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateAddColumnDefaultValue(tablePrefix + enumTableTemp, enumColumnTemp, enumValueTemp[0], true) + `';`
+		sqlcmd += `
+		ELSE
+		`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateCreateTypeEnum(enumNameTemp + "_new", enumValueTemp, true) + `';`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateDropTypeEnum(enumNameTemp) + `';`
+		sqlcmd += `EXECUTE '` + PostgresqlGenerateRenameTypeEnum(enumNameTemp + "_new", enumNameTemp) + `';`
+		sqlcmd += `
+		END IF;
+		END $$;
+		`
 	}
 
 	return sqlcmd, nil
