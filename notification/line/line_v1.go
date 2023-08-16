fc4f63fdd167336ef4a071157eebf8ef43315211
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-08-16 21:50:42 +0700

    add line notify

---
+++ notification/line/line_v1.go
@@ -0,0 +1,51 @@
+package PTGUnotification
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/pkg/errors"
+
+	PTGUhttp "github.com/parinyapt/golang_utils/http/v1"
+)
+
+type LineNotifyInputOption struct {
+	Message              string
+	NotificationDisabled bool
+}
+
+type LineNotifyRequestBodyDataStruct struct {
+	Message              string `form:"message"`
+	NotificationDisabled string `form:"notificationDisabled"`
+}
+
+func LineNotify(lineToken string, param LineNotifyInputOption) (err error) {
+	if lineToken == "" {
+		return errors.New("[Error][PTGUnotification][LineNotify()]->Token is empty")
+	}
+
+	if param.Message == "" {
+		return errors.New("[Error][PTGUnotification][LineNotify()]->Message is empty")
+	}
+
+	_, err = PTGUhttp.HTTPRequest(PTGUhttp.ParamHTTPRequest{
+		RequestTimeout: 5 * time.Second,
+		Type:           PTGUhttp.TypeFormURLEncoded,
+		Method:         http.MethodPost,
+		URL:            "https://notify-api.line.me/api/notify",
+		Headers: map[string]string{
+			"Content-Type":  "application/x-www-form-urlencoded",
+			"Authorization": "Bearer " + lineToken,
+		},
+		Body: LineNotifyRequestBodyDataStruct{
+			Message:              param.Message,
+			NotificationDisabled: strconv.FormatBool(param.NotificationDisabled),
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "[Error][PTGUnotification][LineNotify()]->HTTPRequest error")
+	}
+
+	return nil
+}
